5671929e025534630752ec7b6f9dd404cdcf3c37
Артур Шахвердян <Arturio06@gmail.com>
2023-10-08 14:03:05 +0300

    fix login test

--- src/4features/AuthByUsername/model/slice/loginSlice.test.ts
+++ src/4features/AuthByUsername/model/slice/loginSlice.test.ts
@@ -13,6 +13,6 @@ describe('loginSlice.test', () => {
     const state: DeepPartial<LoginSchema> = { password: '123' };
     expect(
       loginReducer(state as LoginSchema, loginActions.setPassword('123123'))
-    ).toEqual({ username: '123123' });
+    ).toEqual({ password: '123123' });
   });
 });
